0be6d55d36cde51d4bdb1a9212e40c15fefa6d91
mmmmattos13 <mmmattos13@gmail.com>
2024-07-04 16:09:40 -0300

    update cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { router } from './routes';
+import cors from 'cors';
 
 export function createApp(){
     const app = express()
@@ -7,6 +8,8 @@ export function createApp(){
     app.use(express.json())
     
     app.use("/api", router)
+
+    app.use(cors())
 
     return app
 }
